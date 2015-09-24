d099facc0a74787d376a8816bd59278398e90013
Retina Quentin <PQ@Retina-Quentin.local>
2015-09-24 16:40:11 +0200

    mise en place du menu

--- view/header.php
+++ view/header.php
@@ -1,8 +1,8 @@
 <header>
 	<img id="Vaise_logo" src="./assets/img/Vaise_logo.png" href="./" alt="Logo de Vaise, retour vers l'accueil"/>
 	<ul id="menu">
-		<li><a href="./">Accueil</a></li>
-		<li>
+		<li id="home"><a href="./">Accueil</a></li>
+		<li id="everyday">
 			<a href="./">Vie Pratique</a>
 			<ul>
 				<li><a href="./">Se déplacer</a></li>
@@ -12,7 +12,7 @@
 				<li><a href="./">Hôtels</a></li>
 			</ul>
 		</li>
-		<li>
+		<li id="leisure">
 			<a href="/">Loisir</a>
 			<ul>
 				<li><a href="./">Espaces verts</a></a></li>
@@ -21,7 +21,7 @@
 				<li><a href="./">Théâtres</a></li>
 			</ul>
 		</li>
-		<li>
+		<li id="culture">
 			<a href="/">Culture</a>
 			<ul>
 				<li><a href="./">Musées</a></li>
@@ -30,6 +30,6 @@
 				<li><a href="./">Médiathèque</a></li>
 			</ul>
 		</li>
-		<li><a href="./">Liens utiles</a></li>
+		<li id="links"><a href="./">Liens utiles</a></li>
 	</ul>
 </header>
