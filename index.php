5f6fb78ebf1e91e25adf38b03dca3a52b354f7ec
theo <theo.delsante@gmail.com>
2015-09-24 14:20:38 +0200

    Ajout des premiers fichier

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tourisme Vaise</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" type="image/png" href="asset/img/logo-vaise.png"/>
+    </head>
+    
+    <header>
+        <div id="logo">
+            <img src="asset/img/logo-vaise.png" alt="logo-vaise" height="20%" width="20%">
+            <img src="asset/img/" alt="carte-test" name="carte" height="15%" width="15%">
+        </div>
+        
+        <div id="menu">
+            <ul>
+                <li><a href="/">Vie Pratique</a>
+                    <ul>
+                        <li><a href="/"></a></li>
+                        <li><a href="/"></a></li>
+                        <li><a href="/"></a></li>
+                        <li><a href="/"></a></li>
+                        <li><a href="/"></a></li>
+                    </ul>
+                </li>
+                <li><a href="/">Loisirs</a></li>
+                <li><a href="/">Culture</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        </div>
+    </header>
+</html>
