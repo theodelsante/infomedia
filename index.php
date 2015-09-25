47055d8bdae5af08bc05de209a3186f7c14bc033
Retina Quentin <PQ@Retina-Quentin.local>
2015-09-25 18:15:31 +0200

    Ajout Bootstrap 3.3.5

--- index.php
+++ index.php
@@ -19,9 +19,11 @@ require_once('config.php');
 	<meta name="HandheldFriendly" content="true"/>
 
 	<link href="./assets/css/main.css" type="text/css" rel="stylesheet" media="all">
+  <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="./assets/css/print.css" type="text/css" rel="stylesheet" media="print">
 
 	<script src="./assets/js/jQuery.js" type="text/javascript"></script>
+  <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
 	<script src="./assets/js/isotope.min.js"></script>
 	<script src="./assets/js/script.js"></script>
 	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
