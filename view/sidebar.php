fa20908a20358e7701fbe9890d4dcba2cbe88876
Quentame <polletquentin74@me.com>
2015-09-28 10:09:35 +0200

    ... séparation sidebar

---
+++ view/sidebar.php
@@ -0,0 +1,20 @@
+<div class="col-md-3 col-sm-12" id="right">
+  <h2>Actualité</h2>
+  <div>
+    <h3>Le mot du maire</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dui neque, semper ut neque at, interdum ultricies mauris. Donec consectetur nibh sed leo aliquet gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent sodales finibus metus non mattis. Quisque lacinia varius dolor maximus aliquam. Mauris ut sodales magna. In sit amet nisl imperdiet, maximus mauris sit amet, placerat arcu. Curabitur ut magna mattis, dictum tortor sit amet, egestas ex. Nam maximus orci id libero suscipit, at sagittis nunc fringilla.</p>
+  </div>
+  <div>
+    <h3>Météo</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dui neque, semper ut neque at, interdum ultricies mauris. Donec consectetur nibh sed leo aliquet gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+  </div>
+  <div>
+    <h3>Partager</h3>
+    <div class="icon">
+      <i class="fa fa-facebook-square fa-3x"></i>
+      <i class="fa fa-twitter-square fa-3x"></i>
+    </div>
+    <h3>Imprimer</h3>
+    <div class="icon"><i class="fa fa-print fa-3x"></i></div>
+  </div>
+</div>
