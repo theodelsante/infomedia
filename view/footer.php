1be3ce30fc19831b00d9e0f55a5cf93c426d10de
Eifix <marius.stephany@gmail.com>
2015-09-28 18:33:02 +0200

    Ajout du footer

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,22 @@
 <footer>
-	
+    <div class="row liens">
+        <ul>
+            <li><a>Accueil</a></li>
+            <li>|</li>
+            <li><a>Vie pratique</a></li>
+            <li>|</li>
+            <li><a>Loisir</a></li>
+            <li>|</li>
+            <li><a>Culture</a></li>
+            <li>|</li>
+            <li><a>Liens utiles</a></li>
+            <li>|</li>
+            <li><a>Mentions legales</a></li>
+        </ul>
+    </div>
+    <div class="row">
+        <img src="images/lyon9eme.png" alt="Logo Lyon 9" id="logolyon9">
+        <img src="images/grandLyon.png" alt="Logo Grand Lyon" id="logograndlyon">                
+        <p class="texte">Mairie du 9e arrondissement - 6 place du March&eacute; 69009 Lyon - 04 72 19 81 81</p>
+    </div>
 </footer>
