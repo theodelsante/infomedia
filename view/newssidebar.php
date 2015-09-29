bfda411e35ae5a90b12169c216ca0ce09c2726c5
theo <theo.delsante@gmail.com>
2015-09-29 14:45:06 +0200

    Ajout views : 	- news 	- newssidebar

---
+++ view/newssidebar.php
@@ -0,0 +1,21 @@
+<div class="col-md-3 col-sm-12" id="right">
+    <center>
+        <h2>Dernieres News</h2>
+    </center>
+      <div>
+          <img src="./assets/img/news2.jpg">
+      </div>
+      <div>
+        <h3>Météo</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dui neque, semper ut neque at, interdum ultricies mauris. Donec consectetur nibh sed leo aliquet gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+      </div>
+      <div>
+        <h3>Partager</h3>
+        <div class="icon">
+          <i class="fa fa-facebook-square fa-3x"></i>
+          <i class="fa fa-twitter-square fa-3x"></i>
+        </div>
+        <h3>Imprimer</h3>
+        <div class="icon"><i class="fa fa-print fa-3x"></i></div>
+      </div>
+</div>
