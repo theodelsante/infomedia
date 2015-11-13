ac68295338c6a1bca8c7fee825b28e0e567cb4cb
Quentame <polletquentin74@me.com>
2015-11-13 18:29:03 +0100

    Add Build page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$pages = array('home', 'listnews', 'news', 'category', 'sscat', 'usefull_links', 'legal_notice', 'shop', 'product', 'pay');
+$pages = array('home', 'listnews', 'news', 'category', 'sscat', 'usefull_links', 'legal_notice', 'shop', 'product', 'pay', 'build');
 if (isset($_GET['page']) && $_GET['page'] != '') {
   if (in_array($_GET['page'], $pages)) {
     $page = $_GET['page'];
--- view/build.php
+++ view/build.php
@@ -1,8 +1,30 @@
-<div class="row">
-    <div id="build-h2" class="col-xs-12">
-        <h2>Cette page n’est pas encore totalement terminée ...</h2>
+<link href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css" rel="stylesheet" media="all"/>
+<link href="./assets/css/errors.css" type="text/css" rel="stylesheet" media="all"/>
+<div class="yoloclass">
+  <img src="assets/img/Quais_de_Saone.jpg" alt="quais de saone">
+  <div class="yolo" style="color:white">
+    <div class="ribbon">
+      <div class="medallion"></div>
+      <div class="ribbon-1">
+        <span class="inner">
+          <span class="fadeLeft">EN</span>
+        </span>
+      </div>
+      <div class="ribbon-2">
+        <span class="inner">
+          <span class="fadeRight">CONSTRUCTION</span>
+        </span>
+      </div>
+      <div class="ribbon-3">
+        <span class="inner">
+          <span class="fadeLeft">Revenez plus tard</span>
+        </span>
+      </div>
+      <div class="ball fadeIn">
+        <span class="ball-text">
+          <strong><a href="./">Déviation</a></strong>
+        </span>
+      </div>
     </div>
-    <div class="sidebarhor">
-    	<img id="img" src="./assets/img/sidebarho.png"/>
-    </div>
+  </div>
 </div>
