431b596bd405502fa5db3a3ef5028d2e2f32e89d
Quentame <polletquentin74@me.com>
2015-12-01 20:47:10 +0100

    ADD SQL PRIVILEGIES
    - HOW TO -
    1 - DROP TABLE 'G4_infomedia'
    2 - Import the SQL file

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once('controller/pageController.php');
-require_once('../config.php');
+require_once('config.php');
 require_once('controller/controller.php');
 require_once('../controller/langController.php');
 ?>
